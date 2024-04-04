3862bbb9a3cd9747ae893f77c5f0ff56ade00b90
Guillaume Smith <gwil1248@fastmail.net>
2024-04-04 11:28:08 +1100

    Ruff reformating

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='posts'),
-    path("about/", views.about, name="about")
+    path("", views.home, name="posts"),
+    path("about/", views.about, name="about"),
 ]
